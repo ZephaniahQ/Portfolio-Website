90bbf4d5f8f52ca0af948549fd74e41627ff6136
ZephaniahQ <zeph.qamar4@gmail.com>
2024-01-14 07:05:37 +0500

    arduino page is complete

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var projects = document.getElementsByClassName('row');
+  
+  // Adding a click listener to each category element
+  for (var i = 0; i < projects.length; i++) {
+      projects[i].addEventListener('click', function() {
+          // Retrieve the data-href attribute and redirect to the URL
+          var url = this.getAttribute('data-href');
+          window.location.href = url;
+      });
+  }
+});
+
+
 let slideIndex = 0;
 showSlides();
 
@@ -12,3 +26,6 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+
+
